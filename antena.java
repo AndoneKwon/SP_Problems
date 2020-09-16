82e51005135e64b20f78a6e389f4b7ed82526adb
AndoneKwon <45382126+AndoneKwon@users.noreply.github.com>
2020-09-16 13:54:21 +0900

    Create antena.java

---
+++ antena.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int homeNum = Integer.parseInt(br.readLine());
+        int []distance = new int[homeNum];
+        int []homes = new int[homeNum];
+        int i=0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        while (st.hasMoreTokens()){
+            homes[i]=Integer.parseInt(st.nextToken());
+            i++;
+        }
+
+        Arrays.sort(homes);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(Integer.toString(homes[homeNum/2-1]));
+        bw.flush();
+        bw.close();
+    }
+}
