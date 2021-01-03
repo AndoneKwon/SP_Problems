dc5966001c8a0819bf0873dbecbd6b87acb44992
AndoneKwon <45382126+AndoneKwon@users.noreply.github.com>
2021-01-04 01:28:15 +0900

    Update Number_of_1_Bits.java

--- Number_of_1_Bits.java
+++ Number_of_1_Bits.java
@@ -1,6 +1,13 @@
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
-        
+        String number = Integer.toBinaryString(n);
+        int answer = 0;
+        for(int i=0;i<number.length();i++){
+            if(number.charAt(i)=='1')
+                answer++;
+        }
+
+        return answer;
     }
 }
